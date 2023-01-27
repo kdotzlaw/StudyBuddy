ecad413b80ca16b6cd29c95a7fa18886891f8448
Andrea Abellera <andrea.abellera@gmail.com>
2023-01-26 22:19:46 -0600

    Add Vue frontend; Remove duplicate meeting note on root

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import { ref } from "vue"
+
+  const props = defineProps({ 
+    text: {type: String, required: false, default: "Button"}
+  })
+
+  function clickButt(){ yayText.value += "Yay!<br>" }
+  const yayText = ref("")
+
+</script>
+
+<template>
+  <button @click="clickButt"> {{ props.text }} </button>
+  <div v-html="yayText"></div>
+</template>

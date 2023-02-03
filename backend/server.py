3b5ba6d6698c637566519099ae9d6f54497f6f59
rdotzlaw <ryan@dotzlaw.com>
2023-02-03 16:08:33 -0600

    quick fix

--- backend/server.py
+++ backend/server.py
@@ -1,24 +1,3 @@
-<<<<<<< Updated upstream
-import json
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-
-# serve index page
-@app.route('/')
-def serve_index():
-    return "hello world"
-
-
-# receive log-in info from user
-@app.route('/login', methods=['POST'])
-def login():
-    info = json.loads(request.data)
-    username = info.get('username')
-    password = info.get('password')
-    # do stuff
-=======
 import flask
 import flask_login
 

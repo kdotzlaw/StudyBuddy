c13661089d949df254ee4d3230265356ac0a7efd
Andrea Abellera <andrea.abellera@gmail.com>
2023-02-05 14:37:09 -0600

    Link global CSS and Header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
+  import "./global.css"
   import Button from "./components/Button.vue"
+  import Header from "./components/Header.vue"
 </script>
 
 <template>
+  <Header />
   <Button text = "Vue site is up!" />
+
 </template>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,40 @@
+<script setup>
+    // Javascript
+    // Props
+    // Imports
+</script>
+
+<template>
+    <div id="header">
+        <div id="pageNameSection">
+            Page Name
+        </div>
+        <div id="timerSection">
+            Current time 0:00
+        </div>
+        <div id="userSection">
+            Welcome User123!
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    #header{
+        color: wheat;
+        display: grid;
+        grid-template-columns: 25% 1fr 20%;
+        text-align: center;
+    }
+
+    #pageNameSection{
+        background: darkcyan;
+    }
+    
+    #timerSection{
+        background: darkslategray;
+    }
+    
+    #userSection{
+        background: darkgreen;
+    }
+</style>

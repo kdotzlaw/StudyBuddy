bc574022f7fc2cfba1e6e283db063d9318a0e5e7
millandavid <71908868+millandavid@users.noreply.github.com>
2023-03-04 00:49:00 -0600

    Update GradeCalculator.vue
    Added letter grade table

--- frontend/src/pages/GradeCalculator.vue
+++ frontend/src/pages/GradeCalculator.vue
@@ -10,20 +10,64 @@
       </router-link>
     </div>
     
-  <!-- </div> -->
   <div id="grade-container">
     <div id="breakdown-container">
       <h2>Breakdown and Weights</h2>
+      <div id="breakdown-table">
+        <table> 
+          <thead>
+            <tr>
+              <th id="col_one">Type Name</th>
+              <th id="col_two">QTY</th>
+              <th id="col_three">Percentage</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><input type="text" id="name-input" placeholder="Quiz" v-model="username"></td>
+              <td><input type="number" min="0" id="qty-input" placeholder="0" v-model="username"></td>
+              <td><input type="number" min="0" max="100" id="percent-input" placeholder="0" v-model="username"> %</td>
+            </tr>
+            <tr>
+              <td><input type="text" id="name-input" placeholder="Quiz" v-model="username"></td>
+              <td><input type="number" min="0" id="qty-input" placeholder="0" v-model="username"></td>
+              <td><input type="number" min="0" max="100" id="percent-input" placeholder="0" v-model="username"> %</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      
     </div>
     
     <div id="letter-grade-container">
       <h2>Letter Grade Breakdown</h2>
+      <div id="letter-table">
+        <table> 
+          <thead id="letter-table-header">
+            <tr>
+              <th id="col_one">Letter Grade</th>
+              <th id="col_two">Max</th>
+              <th id="col_three">Min</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>A+</td>
+              <td><input type="number" min="0" max="100" id="max-input" placeholder="0" v-model="username"></td>
+              <td><input type="number" min="0" max="100" id="max-input" placeholder="0" v-model="username"> </td>
+            </tr>
+            <tr>
+              <td>A</td>
+              <td><input type="number" min="0" max="100" id="max-input" placeholder="0" v-model="username"></td>
+              <td><input type="number" min="0" max="100" id="max-input" placeholder="0" v-model="username"> </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
 
     <div id="save-button">
-      <button class="button bar">
-        Save Changes
-      </button>
+      <button class="button bar">Save Changes</button>
     </div>
 
   </div>
@@ -106,8 +150,8 @@
   }
 
   .button:hover{
-      transition: 0.3s;
-      background: var(--button-shade);
+    transition: 0.3s;
+    background: var(--button-shade);
   }
 
   .bar{
@@ -115,9 +159,8 @@
   }
 
   #grade-container{
-    width: 70%;
+    width: 50%;
     height: 90%;
-
 
     background: var(--indigo);
     border: 3px solid var(--gold);
@@ -135,15 +178,72 @@
   #breakdown-container{
     width: 70%;
     height: 45%;
-    background-color: red;
     margin: auto;
+  }
+
+  #breakdown-table{
+    width: 70%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  #letter-table{
+    width: 70%;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: var(--box);
+  }
+
+  table{
+    width: 100%;
+    border-collapse: collapse;
+  }
+
+  td{
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+  }
+
+  tr{
+    height: 5vh;
+    justify-content: center;
+  }
+
+  input{
+    font: 500 1rem 'Quicksand', sans-serif;
+    border-radius: 5px;
+    height: 3vh;
+    border-style: none;
+  }
+
+  #col_two, #col_three{
+    width: 25%;
+  }
+
+  #name-input{
+    width: 40%
+  }
+
+  #qty-input, #percent-input{
+    width: 30%;
   }
 
   #letter-grade-container{
     width: 70%;
     height: 45%;
-    background-color:  blue;
     margin: auto;
+  }
+
+  #letter-table-header{
+    background-color: var(--indigo);
+    color: var(--white);
+  }
+
+  #min-input, #max-input{
+    background-color: var(--box);
+    color: var(--white);
+    text-align: center;
   }
 
 </style>

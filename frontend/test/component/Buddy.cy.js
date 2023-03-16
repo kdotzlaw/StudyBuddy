74b604f1056e2e3e05b1b7f9da53b435ab33c4f1
Andrea Abellera <andrea.abellera@gmail.com>
2023-03-15 23:49:37 -0500

    Complete stores tests; Fix Buddy compotest

--- frontend/test/component/Buddy.cy.js
+++ frontend/test/component/Buddy.cy.js
@@ -1,5 +1,7 @@
 import { mount } from 'cypress/vue';
 import Buddy from "../../src/components/Buddy.vue";
+import { setActivePinia, createPinia } from 'pinia';
+setActivePinia(createPinia());
 
 describe('<Buddy />', () => {
   it('renders', () => {

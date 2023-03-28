cb6b51522bc868d8a72a3d97bb1ccdb07909e3b9
Katrina Dotzlaw <dotzlawk@myumanitoba.ca>
2023-03-27 20:13:00 -0500

    clean code

--- backend/locustfile.py
+++ backend/locustfile.py
@@ -5,6 +5,10 @@ from locust import HttpUser, task, between
 3. In backend directory: python -m locust
 4. Visit: http://localhost:8089/
 '''
+
+'''
+Perfoms a load test for login user, get all classes, logout user
+'''
 class WebsiteTestUser(HttpUser):
     wait_time = between(0.5,3)
     creds = {'username': 'ryan2023', 'password': '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'}
@@ -12,10 +16,8 @@ class WebsiteTestUser(HttpUser):
     def on_start(self):
         creds = {'username': 'ryan2023', 'password': '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'}
         self.client.post("http://localhost:5000/api/login", json=creds)
-        #pass
     def on_stop(self):
         self.client.post("http://localhost:5000/api/logout")
-        #pass
     @task
     def getClasses(self):
         self.client.get("http://localhost:5000/api/class")
